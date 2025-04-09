530c2f5e380c7a9349c24e9bbe3d51654e0e7430
Kiiro <ajxx030217@163.com>
2025-04-08 20:52:51 -0400

    feat: parse exif info

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,6 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { NextRequest, NextResponse } from 'next/server'
+import * as exifr from 'exifr'
 
 const s3Client = new S3Client({
   endpoint: process.env.SPACES_ENDPOINT,
@@ -19,38 +20,69 @@ export async function POST(req: NextRequest) {
     )
   }
 
-  const urls: string[] = []
-
   try {
     const formData = await req.formData()
     const files = formData.getAll('files') as File[]
-
-    //TODO
 
     if (!files || files.length === 0) {
       return NextResponse.json({ error: 'No files uploaded' }, { status: 400 })
     }
 
+    const urls: string[] = []
+
     for (const file of files) {
-      const fileBuffer = Buffer.from(await file.arrayBuffer())
+      const arrayBuffer = await file.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
+
+      let latitude: number | null = null
+      let longitude: number | null = null
+      let takenAt: Date | null = null
+
+      try {
+        const exif = await exifr.parse(buffer, { gps: true })
+
+        function roundTo6(num: number | null, digits = 6): number | null {
+          return num !== null ? parseFloat(num.toFixed(digits)) : null;
+        }
+
+        if (exif?.latitude && exif?.longitude) {
+          latitude = roundTo6(exif.latitude)
+          longitude = roundTo6(exif.longitude)
+        }
+
+        if (exif?.DateTimeOriginal) {
+          takenAt = exif.DateTimeOriginal
+        }
+      } catch (exifErr) {
+        console.warn(`Failed to parse EXIF for ${file.name}:`, exifErr)
+      }
+
+      console.log('latitude,longitude, takenAt', latitude, longitude, takenAt)
+
+
+
+
+
       const key = `uploads/${Date.now()}-${file.name}`
 
       const command = new PutObjectCommand({
         Bucket: process.env.SPACES_BUCKET, // next-app-files
         Key: key,
-        Body: fileBuffer,
+        Body: buffer,
         ACL: 'public-read',
         ContentType: file.type || 'application/octet-stream'
       })
 
       await s3Client.send(command)
       const url = `https://${process.env.SPACES_BUCKET}.${process.env.SPACES_REGION}.digitaloceanspaces.com/${key}`
+      urls.push(url)
 
       //TODO
+      // Save the photo metadata to the database
 
-      urls.push(url)
     }
     return NextResponse.json({ urls }, { status: 200 })
+    // return NextResponse.json({ urls }, { status: 200 })
   } catch (error) {
     console.error('Upload error:', error)
     return NextResponse.json(
