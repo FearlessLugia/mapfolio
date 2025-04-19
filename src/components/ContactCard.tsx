89aa6d6d788218ed0771cd905d097b898420ab44
Kiiro <ajxx030217@163.com>
2025-04-18 21:39:08 -0400

    feat: update info in ContactCard

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -10,13 +10,20 @@ export const ContactCard = () => {
           '>
       <CardContent className='flex flex-col items-center space-y-8'>
         <div className='flex flex-col items-center space-y-6 text-center'>
+          <p></p>
+          <p className='uppercase text-sm tracking-widest'>( CONTACT ME )</p>
+          <p className='text-base'>kiiro.huang@outlook.com</p>
+
+          <p></p>
+          <p></p>
+
           <p className='uppercase text-sm tracking-widest'> ( SITE INFORMATION )</p>
 
           <p className='uppercase text-sm tracking-widest'>( TECHNOLOGY )</p>
-          <p className='text-base'> Next.js / TypeScript / Tailwind CSS / GSAP</p>
-
-          <p className='uppercase text-sm tracking-widest'>( MAPFOLIO )</p>
-          <p className='text-base'> Mapbox API / Digital Ocean </p>
+          <p className='text-base'> Next.js / TypeScript </p>
+          <p className='text-base'> Tailwind CSS / GSAP / shadcn</p>
+          <p className='text-base'> PostgreSQL / Supabase / Prisma </p>
+          <p className='text-base'> Mapbox API / Amazon S3 / Digital Ocean </p>
 
           <p className='uppercase text-sm tracking-widest'>( DEPLOY )</p>
           <p className='text-base'>Vercel</p>
