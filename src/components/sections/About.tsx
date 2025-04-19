ed349696829c01d7071daf258430326cdfcf3618
Kiiro <ajxx030217@163.com>
2025-04-19 04:37:32 -0400

    perf: improve responsiveness

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -6,12 +6,14 @@ import MapPreview from '@/components/cards/MapPreview'
 export const About = () => {
   return (
     <section className='container mx-auto px-4 py-24'>
-      <div className='ml-10 grid md:grid-cols-2 items-center'>
+      <div className='grid grid-cols-1 md:grid-cols-2 items-center gap-8'>
         <div>
-          <h2 className='text-3xl font-bold tracking-tight mb-4'>My Photography Journey</h2>
+          <h2 className='text-3xl font-bold tracking-tight mb-4'>
+            My Photography Journey
+          </h2>
           <p className='text-muted-foreground mb-6'>
-            I've captured moments across the globe, from bustling cities to serene landscapes. Each photo tells a
-            story of my adventures and the beautiful places I've visited.
+            I&#39;ve captured moments across the globe, from bustling cities to serene landscapes.
+            Each photo tells a story of my adventures and the beautiful places I&#39;ve visited.
           </p>
           <div className='flex flex-col sm:flex-row gap-4'>
             <Button asChild>
@@ -27,8 +29,10 @@ export const About = () => {
           </div>
         </div>
 
-        <div className='justify-self-end mr-10 rounded-lg overflow-hidden border-3xl w-100 h-100 flex items-center justify-center'>
-          <MapPreview/>
+        <div className="w-full md:w-auto md:h-auto md:justify-self-end ">
+          <div className="w-full h-full md:w-100 md:h-100 rounded-lg overflow-hidden border flex items-center justify-center">
+            <MapPreview />
+          </div>
         </div>
       </div>
     </section>
