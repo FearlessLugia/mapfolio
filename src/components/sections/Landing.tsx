1848d17c3c7d72492b8a46ed3d59604f40da0322
Kiiro <ajxx030217@163.com>
2025-04-20 06:11:08 -0400

    feat: add simple Landing

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -1,5 +1,20 @@
 export const Landing = () => {
   return (
-    <div className='bg-blue-50 h-[calc(100vh-76px)]'></div>
+    <section className='bg-white bg-cover bg-no-repeat bg-center h-[calc(100vh-76px)]'>
+      <div
+        className='sm:px-16 px-6 max-w-7xl mx-auto gap-5 absolute inset-0 flex flex-col justify-center items-center'
+      >
+        <div>
+          <h1
+            className='font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-black'>
+            Hi, I&#39;m <span className='text-[#914eff]'>Kiiro</span>
+          </h1>
+          <p
+            className='text-black font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100'>
+            Full-Stack Web Development Engineer
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
