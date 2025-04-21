4b744666f7d21e3f90758ef2274cff0b979f58ed
Kiiro <ajxx030217@163.com>
2025-04-20 21:17:41 -0400

    feat: add log when prisma query

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
-export const db = new PrismaClient()
+const globalForPrisma = global as typeof global & { prisma?: PrismaClient }
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({ log: ['query'] })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
