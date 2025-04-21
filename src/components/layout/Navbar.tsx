1f73eb04584d7079130cdaef1d9bac5664605aab
Kiiro <ajxx030217@163.com>
2025-04-20 21:26:50 -0400

    feat: hide upload and admin button when production

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,6 +8,8 @@ export const Navbar = async () => {
     headers: await headers()
   })
 
+  const isProduction = process.env.NODE_ENV === 'production'
+
   return (
     <NavigationMenu className='mx-auto w-full max-w-4xl bg-white'>
       <NavigationMenuList className='flex justify-center items-center p-4 gap-8'>
@@ -29,13 +31,13 @@ export const Navbar = async () => {
             Map
           </NavigationMenuLink>
         </Link>
-        {session && <Link href='/upload' legacyBehavior passHref>
+        {!isProduction && session && <Link href='/upload' legacyBehavior passHref>
           <NavigationMenuLink
-            className='text-lg px-6 py-2 rounded hover:bg-gray-100 transition'>
+            className='text-lg px-6 py-2 rounded hnover:bg-gray-100 transition'>
             Upload
           </NavigationMenuLink>
         </Link>}
-        {session && <Link href='/admin' legacyBehavior passHref>
+        {!isProduction && session && <Link href='/admin' legacyBehavior passHref>
           <NavigationMenuLink
             className='text-lg px-6 py-2 rounded hover:bg-gray-100 transition'>
             Admin
