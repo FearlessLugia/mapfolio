fb6ecbd838239ae40204e5f8a74e16955e9c54a2
Kiiro <ajxx030217@163.com>
2026-05-11 20:04:10 -0400

    fix: update photo location keys

--- src/lib/nearby.ts
+++ src/lib/nearby.ts
@@ -5,14 +5,14 @@ export function getNearbyPhotos(lat: number, lng: number, radiusKm = 5) {
     SELECT *
     FROM "Photo"
     WHERE (
-              ("photoLocation" ->>'lat')::float8 IS NOT NULL AND
-              ("photoLocation"->>'lng')::float8 IS NOT NULL
+              ("photoLocation" ->>'latitude')::float8 IS NOT NULL AND
+              ("photoLocation"->>'longitude')::float8 IS NOT NULL
           )
     AND (
       6371 * acos(
-        cos(radians($1)) * cos(radians(("photoLocation"->>'lat')::float8)) *
-          cos(radians(("photoLocation"->>'lng')::float8) - radians($2)) +
-          sin(radians($1)) * sin(radians(("photoLocation"->>'lat')::float8))
+        cos(radians($1)) * cos(radians(("photoLocation"->>'latitude')::float8)) *
+          cos(radians(("photoLocation"->>'longitude')::float8) - radians($2)) +
+          sin(radians($1)) * sin(radians(("photoLocation"->>'latitude')::float8))
           )
           ) <= $3
   `, lat, lng, radiusKm)
